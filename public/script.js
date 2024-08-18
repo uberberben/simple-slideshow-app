966df09fea263129a39ba3ea1170e488c0975016
Lukasz-Wywial-PVD364_moto <lukasz.wywial@motorolasolutions.com>
2024-08-18 13:12:19 +0200

    CSS update

--- public/script.js
+++ public/script.js
@@ -13,9 +13,18 @@ function createSlide(src) {
     return slide;
 }
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 async function showSlides(timeout) {
     const slideshowContainer = document.getElementById('slideshow-container');
-    const images = await fetchImages();
+    let images = await fetchImages();
+    images = shuffleArray(images);
     images.forEach(image => {
         const slide = createSlide(image);
         slideshowContainer.appendChild(slide);
